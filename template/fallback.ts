87b1e13db11a10992e827fc6bafb6c44655259cd
Ziphil <yfe23457@nifty.com>
2023-02-13 08:04:40 +0900

    変更

--- template/fallback.ts
+++ template/fallback.ts
@@ -5,10 +5,6 @@ import {
 } from "../generator/transformer";
 
 
-const INLINE_TAG_NAMES = ["x", "xn", "a"];
-const PARENT_TRIM_TAG_NAMES = ["li"];
-const PREVIOUS_SIBLING_TRIM_TAG_NAMES = ["label"];
-
 const manager = new AvendiaTemplateManager();
 
 manager.registerElementRule(true, true, (transformer, document, element) => {
@@ -24,31 +20,7 @@ manager.registerElementRule(true, true, (transformer, document, element) => {
 });
 
 manager.registerTextRule(true, (transformer, document, text) => {
-  let content = text.data;
-  content = content.replace(/\uFEFF/gu, "");
-  content = content.replace(/(、|。|」|』|〉)/g, (match) => match + " ");
-  content = content.replace(/(「|『|〈)/g, (match) => " " + match);
-  content = content.replace(/(、|。)\s+(」|』|〉)/g, (match, before, after) => before + after);
-  content = content.replace(/(」|』|〉)\s+(、|。|,|\.)/g, (match, before, after) => before + after);
-  content = content.replace(/(\(|「|『|〈)\s+(「|『|〈)/g, (match, before, after) => before + after);
-  if (!text.previousSibling?.isElement() || !INLINE_TAG_NAMES.includes(text.previousSibling.tagName)) {
-    content = content.replace(/^\s+(「|『|〈)/g, (match, paren) => paren);
-  }
-  if (!text.nextSibling?.isElement() || !INLINE_TAG_NAMES.includes(text.nextSibling.tagName)) {
-    content = content.replace(/(」|』|〉)\s+$/g, (match, paren) => paren);
-  }
-  if (text.parentNode !== null && text.parentNode.isElement()) {
-    const parent = text.parentNode;
-    if (PARENT_TRIM_TAG_NAMES.includes(parent.tagName) && parent.firstChild === text) {
-      content = content.trimStart();
-    }
-  }
-  if (text.previousSibling !== null && text.previousSibling.isElement()) {
-    const previousSibling = text.previousSibling;
-    if (PREVIOUS_SIBLING_TRIM_TAG_NAMES.includes(previousSibling.tagName)) {
-      content = content.replace(/^\s+/g, "");
-    }
-  }
+  const content = text.data;
   const self = document.createTextNode(content);
   return self;
 });
